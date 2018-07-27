be7e8f2a643b4fcdc821d12a17da9f9de69a9905
Johan Duran <johan.durancerdas@gmail.com>
2018-07-27 21:44:48 +0000

    Update index_cicd_tutorial.js

--- app/index_cicd_tutorial.js
+++ app/index_cicd_tutorial.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app     = express();
 
 app.get("/", function(req, res) {
-  res.status(200).send("Hello From Gitlab CI CD Tutorial");
+  res.status(400).send("Hello From Gitlab CI CD Tutorial");
 });
 
 app.listen(8080);
