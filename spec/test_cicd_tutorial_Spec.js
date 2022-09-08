005abc062c9d845f94bcdbfa8cea0e2e1d5688df
frinod <113128306+frinod@users.noreply.github.com>
2022-09-08 21:53:02 +0530

    Update test_cicd_tutorial_Spec.js

--- spec/test_cicd_tutorial_Spec.js
+++ spec/test_cicd_tutorial_Spec.js
@@ -2,7 +2,7 @@ var request = require("request");
 
 var base_url = "http://localhost:8080/"
 
-describe("Hello Welcome page", function() {
+describe("Hello Welcome", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
       request.get(base_url, function(error, response, body) {
